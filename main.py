d731dd866a4fc55c2af26e78f9300241117d7b63
carlosf <8carlosf@gmail.com>
2017-03-15 18:59:07 +0000

    print N, K for easier output parse, reformated code

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from sys import argv
 from random import random, randint
 from math import factorial, e
 
-unm = lambda n, m, b: ((m**n)/(factorial(m)*b))/e
+unm = lambda n, m, b: ((m**n) / (factorial(m) * b)) / e
+
 
 def genBellNumbers(n):
-    B = [0]*(n+1) # Bell Numbers
-    S = [[0]*(n+1) for i in range(n+1)] # Stirling numbers of the second kind
+    B = [0] * (n + 1)  # Bell Numbers
+    # Stirling numbers of the second kind
+    S = [[0] * (n + 1) for i in range(n + 1)]
     S[0][1] = 1
     B[0] = 1
-    for i in range(1, n+1):
-        for j in range(1, i+1):
-            S[i][j] = S[i-1][j]*j + S[i-1][j-1]
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            S[i][j] = S[i - 1][j] * j + S[i - 1][j - 1]
             B[i] += S[i][j]
-        S[i-1] = [] # clean Stirling numbers (ram management)
+        S[i - 1] = []  # clean Stirling numbers (ram management)
     return B
+
 
 def getBellNumber(n):
     B = genBellNumbers(n)
     return B[n]
+
 
 def getM(n, b):
     p = random()
@@ -32,14 +36,17 @@ def getM(n, b):
 
 # 1 - choose M from unm (getM)
 # 2 - Drop n labelled balls uniformly into M boxes
-# 3 - Form a set partition λ of [n] with i and j in the same block if and only if balls i and j are in the same box
+# 3 - Form a set partition λ of [n] with i and j in the same block if and
+# only if balls i and j are in the same box
+
+
 def getRandomSet(n, b):
     m = getM(n, b)
     b = []
     for i in range(n):
-        b += [randint(0, m-1)]
-    
-    norm = [-1]*m
+        b += [randint(0, m - 1)]
+
+    norm = [-1] * m
     normC = 0
     for i in range(n):
         if norm[b[i]] < 0:
@@ -58,5 +65,6 @@ if __name__ == "__main__":
     N = int(argv[1])
     K = int(argv[2])
     b = getBellNumber(N)
+    print(N, K)
     for i in range(K):
         print(' '.join(map(str, getRandomSet(N, b))))
